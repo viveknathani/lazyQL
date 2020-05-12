a65a7396fe7eab1568788627575c5fd373bd643b
viveknathani <viveknathani2402@gmail.com>
2020-05-12 14:37:11 +0530

    Regex Done

--- models/dbhandle.js
+++ models/dbhandle.js
@@ -10,27 +10,30 @@ const connectionHandler = mysql.createConnection(
 
 function createDatabase(databaseName)
 {
+    //removing trailing whitespaces
+    databaseName = databaseName.replace(/\s+$/, '');
+
+    //regular expression for a database name
+    let regex = /^[0-9a-zA-Z$_]+$/g;
+
+    //validate
+    let nameIsValid = regex.test(databaseName);
+
     connectionHandler.connect(function(err)
     {
-        if(err) throw err;
-
-        console.log('In connection with MySQL...');
-
-        let dbQuery=`create database ${databaseName}`;
-
-        connectionHandler.query(dbQuery, function(err, result) 
+        if(nameIsValid)
         {
-            if(err)
+            console.log('In connection with MySQL...');
+            let dbQuery=`create database ${databaseName}`;
+            connectionHandler.query(dbQuery, function(err, result) 
             {
-                console.log('Error in creating the database.');
-            }
-            else 
-            {
-                console.log('Database created.');
-            }
-        } );
-    
+                if(err) console.log('Name already exists.');
+                else console.log('Database created.');
+            } );
+        }
+        else console.log('Invalid name for a database');
     } );
 }
 
+createDatabase('vivek');
 module.exports = { createDatabase : createDatabase };
