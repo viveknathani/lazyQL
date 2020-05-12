4e9b42ab8a5a6e0347bb894d60500ea9682d9875
viveknathani <viveknathani2402@gmail.com>
2020-05-12 20:20:25 +0530

    Handle GET / POST

--- controllers/homepagecontrol.js
+++ controllers/homepagecontrol.js
@@ -1,15 +1,32 @@
 const databaseHandler = require('../models/dbhandle');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app)
 {
     app.get('/', function(req, res)
     {
-        res.render('home');
+        let text = ' ';
+        let namesList=[];
+        databaseHandler.getList(namesList);
+        setTimeout(function()
+                    {  
+                        res.render('home', {name:text, dbnames:namesList} );
+                    }, 10); 
     } );
 
-    app.post('/', function(req, res)
+    app.post('/', urlencodedParser,function(req, res)
     {
-
+        let namesList=[];
+        let databaseName = req.body.db_name;
+        let paragraph = { text : " "};
+        databaseHandler.createDatabase(databaseName, paragraph);
+        setTimeout(function() 
+                    {
+                        databaseHandler.getList(namesList);
+                        console.log(`controllerTime : ${paragraph.text}`);
+                        setTimeout(() => { res.render('home', {name:paragraph.text, dbnames:namesList}); }, 30);
+                    }, 20);
     } );
 
     app.delete('/', function(req, res)
@@ -17,5 +34,3 @@ module.exports = function(app)
 
     } );
 }
-
-databaseHandler.createDatabase('db');
