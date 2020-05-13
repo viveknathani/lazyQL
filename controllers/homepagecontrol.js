c70a7de3bf7f363d529d01e492704045955cdc6b
viveknathani <viveknathani2402@gmail.com>
2020-05-13 18:58:05 +0530

    POST /filledform

--- controllers/homepagecontrol.js
+++ controllers/homepagecontrol.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const databaseHandler = require('../models/dbhandle');
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -27,6 +29,17 @@ module.exports = function(app)
                         console.log(`controllerTime : ${paragraph.text}`);
                         setTimeout(() => { res.render('home', {name:paragraph.text, dbnames:namesList}); }, 30);
                     }, 20);
+    } );
+
+    app.post('/filledform', urlencodedParser, function(req, res) 
+    {
+        databaseHandler.createTable(req.body);
+        res.send('okay bye!');
+    } );
+
+    app.post('/:dbname', urlencodedParser, function(req, res) 
+    {
+        res.render('table');
     } );
 
     app.delete('/', function(req, res)
