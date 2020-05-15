c114e87cddeea48206db02a3ba01fa296ea45e6a
viveknathani <viveknathani2402@gmail.com>
2020-05-15 21:32:09 +0530

    Clear up

--- controllers/homepagecontrol.js
+++ controllers/homepagecontrol.js
@@ -7,6 +7,8 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app)
 {
+    //homepage GET request
+    //collects all the databases and tables and sends them to the view
     app.get('/', function(req, res)
     {
         let text = ' ';
@@ -17,6 +19,8 @@ module.exports = function(app)
             tablesList[i]=new Array();
         }
         databaseHandler.getList(namesList);
+
+        //wait for the server to send the list
         setTimeout(function()
                     {   
                         setTimeout(function()
@@ -24,12 +28,15 @@ module.exports = function(app)
                             databaseHandler.getTables(namesList, tablesList);
                         }, 50);
                     }, 100); 
+
+        //wait for server and render            
         setTimeout(function()
                     {
                         res.render('home', {name:text, dbnames:namesList, Tables:tablesList} );
                     }, 200);            
     } );
 
+    //GET a specific table
     app.get('/:dbname/:tablename', urlencodedParser, function(req, res) 
     {
         let rowsList=[]
@@ -37,6 +44,7 @@ module.exports = function(app)
         databaseHandler.fetchTables(req.params.dbname, req.params.tablename, rowsList);
         setTimeout(() => 
         {
+            //if table is not empty
             if(typeof rowsList != "undefined" && rowsList != null && rowsList != null
             && rowsList.length > 0)
             {
@@ -51,6 +59,7 @@ module.exports = function(app)
                         }), 20);
             }
 
+            //separate view for empty tables
             else
             { 
                databaseHandler.getColumnNames(req.params.dbname, req.params.tablename, columns);
@@ -64,6 +73,7 @@ module.exports = function(app)
         }, 10);
     } );
 
+    //POST table data for insert operation
     app.post('/insert/:dbname/:tablename', urlencodedParser, function(req, res) 
     {
         console.log(req.body);
@@ -71,6 +81,7 @@ module.exports = function(app)
         res.redirect(`/${req.params.dbname}/${req.params.tablename}`);
     } );
 
+    //POST database name and receive appropriate message from the server
     app.post('/', urlencodedParser,function(req, res)
     {
         let namesList=[];
@@ -82,6 +93,7 @@ module.exports = function(app)
         {
             tablesList[i]=new Array();
         }
+
         setTimeout(function() 
                     {
                         databaseHandler.getList(namesList);
@@ -101,23 +113,27 @@ module.exports = function(app)
         }, 200);
     } );
 
+    //POST table information and perform create table operation in the server
     app.post('/filledform', urlencodedParser, function(req, res) 
     {
         databaseHandler.createTable(req.body);
         res.sendFile('CreateTable.html', {root : 'public/assets'});
     } );
 
+    //POST request for generating .sql file, runs a simple script located in ./scripts
     app.post('/generate/:dbname', urlencodedParser, function(req, res) 
     {
         fileGenerator.generateSQLFile(req.params.dbname);
         res.sendFile('File.html', {root : 'public/assets'});
     } );
 
+    //avail a form to fill information about a table
     app.post('/:dbname', urlencodedParser, function(req, res) 
     {
         res.render('table');
     } );
 
+    //yet to handle DELETE requests
     app.delete('/', function(req, res)
     {
 
